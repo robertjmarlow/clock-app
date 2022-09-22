b90764a6dba703612a7f54ac55301bfd41435168
robertjmarlow <robertjmarlow@gmail.com>
2022-09-21 21:34:17 -0500

    Adding important "go to the past or future" functionality

--- src/main/kotlin/com/marlowsoft/timeserver/TimeControllers.kt
+++ src/main/kotlin/com/marlowsoft/timeserver/TimeControllers.kt
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
+import java.time.Period
 import java.time.ZonedDateTime
+import java.time.format.DateTimeParseException
 import java.util.TimeZone
 
 @RestController
@@ -16,19 +18,43 @@ import java.util.TimeZone
 class TimeControllers {
     companion object {
         val KNOWN_TIMEZONES: Set<String> = ImmutableSet.copyOf(TimeZone.getAvailableIDs())
+        const val FORWARD_PERIOD_FORMAT = "P%s"
+        const val BACKWARD_PERIOD_FORMAT = "-P%s"
     }
 
-    // dumb stuff to add:
-    //  get time in the past or future
     @GetMapping("/time")
-    fun getTime(@RequestParam tz: String?): TimeResponse {
-        if (tz != null) {
+    fun getTime(@RequestParam tz: String?, @RequestParam future: String?, @RequestParam past: String?): TimeResponse {
+        if (future != null && past != null) {
+            throw ResponseStatusException(HttpStatus.BAD_REQUEST, "Either \"future\" or \"past\" can be populated, but not both")
+        }
+
+        var curTime = if (tz != null) {
             if (!KNOWN_TIMEZONES.contains(tz)) {
                 throw ResponseStatusException(HttpStatus.BAD_REQUEST, String.format("Time zone \"%s\" not recognized", tz))
             }
-            val zoneId = TimeZone.getTimeZone(tz).toZoneId()
-            return TimeResponse(ZonedDateTime.now(zoneId))
+            ZonedDateTime.now(TimeZone.getTimeZone(tz).toZoneId())
+        } else {
+            ZonedDateTime.now()
         }
-        return TimeResponse(ZonedDateTime.now())
+
+        if (future != null || past != null) {
+            try {
+                val period: Period = if (future != null) {
+                    Period.parse(String.format(FORWARD_PERIOD_FORMAT, future))
+                } else {
+                    Period.parse(String.format(BACKWARD_PERIOD_FORMAT, past))
+                }
+                curTime = curTime.plus(period)
+            } catch (e: DateTimeParseException) {
+                val errorMessage: String = if (future != null) {
+                    String.format("Time format \"%s\" not recognized for future parameter", future)
+                } else {
+                    String.format("Time format \"%s\" not recognized for past parameter", past)
+                }
+                throw ResponseStatusException(HttpStatus.BAD_REQUEST, errorMessage)
+            }
+        }
+
+        return TimeResponse(curTime)
     }
 }
--- src/test/kotlin/com/marlowsoft/timeserver/TimeControllersTests.kt
+++ src/test/kotlin/com/marlowsoft/timeserver/TimeControllersTests.kt
@@ -15,6 +15,7 @@ import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import java.time.Period
 import java.time.ZoneId
 import java.time.ZonedDateTime
 
@@ -81,6 +82,61 @@ class TimeControllersTests(@Autowired val mockMvc: MockMvc) {
     }
 
     @Test
+    fun `Get current date and time but a month in the past`() {
+        val nowButAMonthAgo = ZonedDateTime.now().minus(Period.ofMonths(1))
+
+        val aMonthAgoDateTimeJson = mockMvc.perform(get("/api/time").queryParam("past", "1M"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn()
+            .response
+            .contentAsString
+
+        val aMonthAgoDateTimeObj: TimeResponse = jacksonObjectMapper.readValue(aMonthAgoDateTimeJson)
+        val aMonthAgoDateTime = ZonedDateTime.parse(aMonthAgoDateTimeObj.timeStr)
+
+        assertTrue(nowButAMonthAgo.isBefore(aMonthAgoDateTime))
+    }
+
+    @Test
+    fun `Get current date and time but a month in the future`() {
+        val nowButAMonthInTheFuture = ZonedDateTime.now().plus(Period.ofMonths(1))
+
+        val aMonthInTheFutureDateTimeJson = mockMvc.perform(get("/api/time").queryParam("future", "1M"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn()
+            .response
+            .contentAsString
+
+        val aMonthInTheFutureDateTimeObj: TimeResponse = jacksonObjectMapper.readValue(aMonthInTheFutureDateTimeJson)
+        val aMonthInTheFutureDateTime = ZonedDateTime.parse(aMonthInTheFutureDateTimeObj.timeStr)
+
+        assertTrue(nowButAMonthInTheFuture.isBefore(aMonthInTheFutureDateTime))
+    }
+
+    @Test
+    fun `Get current date and time but a year in the past in Japan`() {
+        val tokyoZoneStr = "Asia/Tokyo"
+        val tokyoZoneId = ZoneId.of(tokyoZoneStr)
+        val nowButAYearAgo = ZonedDateTime.now(tokyoZoneId).minus(Period.ofYears(1))
+
+        val aYearAgoDateTimeJson = mockMvc.perform(get("/api/time").queryParam("tz", tokyoZoneStr).queryParam("past", "1Y"))
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andReturn()
+            .response
+            .contentAsString
+
+        val aYearAgoDateTimeObj: TimeResponse = jacksonObjectMapper.readValue(aYearAgoDateTimeJson)
+        val aYearAgoDateTime = ZonedDateTime.parse(aYearAgoDateTimeObj.timeStr)
+
+        // assumption: the REST call isn't instantaneous
+        assertTrue(nowButAYearAgo.isBefore(aYearAgoDateTime))
+        assertEquals(tokyoZoneId, aYearAgoDateTime.zone)
+    }
+
+    @Test
     fun `Get current date and time in a time zone that doesn't exist`() {
         val badTimeZoneStr = "The/Moon"
 
@@ -91,5 +147,38 @@ class TimeControllersTests(@Autowired val mockMvc: MockMvc) {
             .errorMessage
 
         assertEquals("Time zone \"The/Moon\" not recognized", responseStr)
+    }
+
+    @Test
+    fun `Try to set a future and past query param`() {
+        val responseStr = mockMvc.perform(get("/api/time").queryParam("future", "1Y").queryParam("past", "1Y"))
+            .andExpect(status().isBadRequest)
+            .andReturn()
+            .response
+            .errorMessage
+
+        assertEquals("Either \"future\" or \"past\" can be populated, but not both", responseStr)
+    }
+
+    @Test
+    fun `Try to set an invalid future query param`() {
+        val responseStr = mockMvc.perform(get("/api/time").queryParam("future", "1Year"))
+            .andExpect(status().isBadRequest)
+            .andReturn()
+            .response
+            .errorMessage
+
+        assertEquals("Time format \"1Year\" not recognized for future parameter", responseStr)
+    }
+
+    @Test
+    fun `Try to set an invalid past query param`() {
+        val responseStr = mockMvc.perform(get("/api/time").queryParam("past", "1Year"))
+            .andExpect(status().isBadRequest)
+            .andReturn()
+            .response
+            .errorMessage
+
+        assertEquals("Time format \"1Year\" not recognized for past parameter", responseStr)
     }
 }
